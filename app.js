df45377c31f1a0639bb9ff42e0b6e0281558657e
Rohit Arora <rohitarora9627@gmail.com>
2021-09-11 16:47:42 +0530

    delete api

--- app.js
+++ app.js
@@ -2,27 +2,24 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const port = process.env.PORT||4000;
+const app = express();
+app.use(cors())
 
 const connectDB = require('./db/conn');
 dotenv.config({ path: './config/config.env' });
-
 connectDB();
 
 const auth = require('./routes/auth.routes');
 const users = require('./routes/user.routes')
 const categories = require('./routes/category.routes')
 const product = require('./routes/product.routes')
-const app = express();
 // Express Configurations
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({
-    origin:"*",
-}));
 // app.use(allowCrossDomain)
 app.use(express.json());
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers',
+//     res.header('Access-Control-Allow-Headers', 
 //      'Origin,X-Requested-With,Content-Type, Accept, Authorization');
 //     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 //     next()
